46590d4223f68bab0978f26b975e110b783cd975
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-02-07 18:55:33 +0200

    update slider settings

--- script/main.js
+++ script/main.js
@@ -6,23 +6,11 @@ var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.arrow',
     },
-    // Responsive breakpoints
     breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 40
-      }
+        // when window width is >= 540px
+        540: {
+          slidesPerView: 2
+        }
     }
   })
 
